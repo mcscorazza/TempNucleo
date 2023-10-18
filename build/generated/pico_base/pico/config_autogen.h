4f060411481ddef3935972b7d7a893f9597c5a80
mcscorazza <mcs.corazza@gmail.com>
2023-10-18 16:42:58 -0300

    nova versão feita no Mini PC

--- build/generated/pico_base/pico/config_autogen.h
+++ build/generated/pico_base/pico/config_autogen.h
@@ -4,8 +4,8 @@
 
 // based on PICO_CONFIG_HEADER_FILES:
 
-#include "C:/sdk/pico-sdk/src/boards/include/boards/pico_w.h"
+#include "C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/pico-sdk/src/boards/include/boards/pico_w.h"
 
 // based on PICO_RP2040_CONFIG_HEADER_FILES:
 
-#include "C:/sdk/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+#include "C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
--- build/pico-sdk/cmake_install.cmake
+++ build/pico-sdk/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/sdk/pico-sdk
+# Install script for directory: C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/pico-sdk
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -34,14 +34,14 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/bins/arm-gnu/bin/arm-none-eabi-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/Raspberry Pi/Pico SDK v1.5.1/gcc-arm-none-eabi/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/dev/unisal/Arquitetura/TempNucleo/build/pico-sdk/tools/cmake_install.cmake")
-  include("C:/dev/unisal/Arquitetura/TempNucleo/build/pico-sdk/src/cmake_install.cmake")
-  include("C:/dev/unisal/Arquitetura/TempNucleo/build/pico-sdk/docs/cmake_install.cmake")
+  include("C:/dev/unisal/arquitetura/TempNucleo/build/pico-sdk/tools/cmake_install.cmake")
+  include("C:/dev/unisal/arquitetura/TempNucleo/build/pico-sdk/src/cmake_install.cmake")
+  include("C:/dev/unisal/arquitetura/TempNucleo/build/pico-sdk/docs/cmake_install.cmake")
 
 endif()
 
